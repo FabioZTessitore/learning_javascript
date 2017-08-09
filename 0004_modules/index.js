93ee24358e254d99667eed8da2ed93c196acb8f0
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-08-09 22:08:02 +0200

    using modules

---
+++ 0004_modules/index.js
@@ -0,0 +1,14 @@
+// index.js
+
+// using a module
+
+// load lodash module
+// _ is its standard name
+const _ = require('lodash');
+
+// using some functions from lodash module
+const msg = "hello, world!";
+console.log('msg: ' + msg);
+console.log('capitalized: ' + _.capitalize(msg));
+console.log('camel case: ' + _.camelCase(msg));
+console.log('start case: ' + _.startCase(msg));
